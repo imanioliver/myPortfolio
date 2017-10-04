e78079c4a122722409b9ff3e718920cf09bc0a79
Imani Oliver <isaac@ihardy.co>
2017-10-04 11:10:03 -0400

    adds data files for contacts

---
+++ src/components/data.js
@@ -0,0 +1,17 @@
+const contacts = [
+     {
+        name:"Lindsey Owings",
+        email:"lindseyowings@gmail.com",
+        company: "The Iron Yard"
+     },
+     {
+        name:"Lindsay Sutton",
+        email:"lindsaysutton@gmail.com",
+        company: "The Iron Yard"
+     },
+     {
+        name:"Isabel Marques",
+        email:"isabelmarques@gmail.com",
+        company: "The Iron Yard"
+     }
+]
